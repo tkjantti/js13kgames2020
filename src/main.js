3fdfebfb8687ee71fd0cfb3009c027891ea82caa
Tero Jäntti <tero.jantti@tieto.com>
2020-08-23 16:50:59 +0300

    Kontra library added.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,66 @@
-// main.js
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Tero Jäntti, Sami H
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import { init, Sprite, GameLoop } from "kontra";
+
+let { canvas } = init();
+
+const resize = () => {
+  canvas.width = window.innerWidth - 10;
+  canvas.height = window.innerHeight - 10;
+};
+
+window.addEventListener("resize", resize, false);
+resize();
+
+let sprite = Sprite({
+  x: 100, // starting x,y position of the sprite
+  y: 80,
+  color: "red", // fill color of the sprite rectangle
+  width: 20, // width and height of the sprite rectangle
+  height: 40,
+  dx: 2 // move the sprite 2px to the right every frame
+});
+
+let loop = GameLoop({
+  // create the main game loop
+  update: function() {
+    // update the game state
+    sprite.update();
+
+    // wrap the sprites position when it reaches
+    // the edge of the screen
+    if (sprite.x > canvas.width) {
+      sprite.x = -sprite.width;
+    }
+  },
+  render: function() {
+    // render the game state
+    sprite.render();
+  }
+});
+
+loop.start(); // start the game
