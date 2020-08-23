f48890d2f561601f3662d6e69ae7654acead197c
Tero Jäntti <tero.jantti@tieto.com>
2020-08-23 23:22:36 +0300

    A room with bounds for player.

--- src/main.js
+++ src/main.js
@@ -28,7 +28,10 @@ import { init, initKeys, Sprite, GameLoop, keyPressed } from "kontra";
 
 const PLAYER_SPEED = 5;
 
-let { canvas } = init();
+const ROOM_WIDTH = 300;
+const ROOM_HEIGHT = 300;
+
+const { canvas, context } = init();
 initKeys();
 
 const resize = () => {
@@ -39,31 +42,78 @@ const resize = () => {
 window.addEventListener("resize", resize, false);
 resize();
 
-let player = Sprite({
+class Room {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.right = this.x + ROOM_WIDTH;
+    this.bottom = this.y + ROOM_HEIGHT;
+  }
+
+  render() {
+    context.save();
+    context.strokeStyle = "white";
+    context.lineWidth = 5;
+    context.beginPath();
+    context.lineTo(this.x, this.y);
+    context.lineTo(this.x + ROOM_WIDTH, this.y);
+    context.lineTo(this.x + ROOM_WIDTH, this.y + ROOM_HEIGHT);
+    context.lineTo(this.x, this.y + ROOM_HEIGHT);
+    context.closePath();
+    context.stroke();
+    context.restore();
+  }
+}
+
+const player = Sprite({
   x: 100,
   y: 80,
   color: "red",
   width: 20,
-  height: 40
-});
+  height: 40,
 
-let loop = GameLoop({
-  update: function() {
-    player.update();
+  update(room) {
+    let dx = 0,
+      dy = 0;
 
     if (keyPressed("left")) {
-      player.x -= PLAYER_SPEED;
+      dx -= PLAYER_SPEED;
     } else if (keyPressed("right")) {
-      player.x += PLAYER_SPEED;
+      dx += PLAYER_SPEED;
     }
 
     if (keyPressed("up")) {
-      player.y -= PLAYER_SPEED;
+      dy -= PLAYER_SPEED;
     } else if (keyPressed("down")) {
-      player.y += PLAYER_SPEED;
+      dy += PLAYER_SPEED;
     }
+
+    if (this.x + dx < room.x) {
+      this.x = room.x;
+    } else if (room.right < this.x + dx + this.width) {
+      this.x = room.right - this.width;
+    } else {
+      this.x += dx;
+    }
+
+    if (this.y + dy < room.y) {
+      this.y = room.y;
+    } else if (room.bottom < this.y + dy + this.height) {
+      this.y = room.bottom - this.height;
+    } else {
+      this.y += dy;
+    }
+  }
+});
+
+const room = new Room(30, 30);
+
+const loop = GameLoop({
+  update: function() {
+    player.update(room);
   },
   render: function() {
+    room.render();
     player.render();
   }
 });
